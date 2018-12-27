e3d2dbb021a757981b434a4152d5222abe5e3b13
Ginks <pissabruney@hotmail.com>
2018-12-27 17:34:39 +0100

    Update bsp.ml

--- bsp.ml
+++ bsp.ml
@@ -8,66 +8,109 @@ let rec hauteur = function
 	| L(_,g,d) -> 1 + (max (hauteur g) (hauteur d));;
 	
 
-(* Ajoute un label dans un bsp *)
+(*fonction ramdom qui determine quel noeud contruire, 0->vertical et 1->horizontal*)
+let random_noeud() = match (Random.int(2)) with
+  |0 -> "vert"
+  |_ -> "hor"
+;;
 
-let ajout e b = 
-	let rec add n bs i= 
-		match b with
-		| R(a) -> L(e, R None, R None)
-		(* verticale *)
-		| L(l,g,d) -> if (i mod 2) <>0 then L(l, (add n g (hauteur bs)), d)
-					else L(l, g, (add n d (hauteur bs)))
-	in add e b (hauteur b)
+(*fonction ramdom qui determinera vers quel fils aller, 0-->fils gauche et 1->fils droite*)
+let leftorright() = Random.int(2);;
+
+(*fonction qui construit une feuille d'un arbre bsp, prend un string(noeud)->type du noeud, un int(x)->valeur à ajouter, un arbre bsp(a)*)
+let ajout_Bsp noeud x a =
+  let rec aux x a parite = match a with
+    |R _ -> L({coord = x; colored = false}, R None, R None)
+    |L(c, l, r) ->
+      if noeud = "vert" then
+	 if (parite mod 2) = 0 then
+	   if c.coord = x then a
+	   else if x<c.coord then match l with
+	    |R _ -> a
+	    |L(,,_) -> L(c, aux x l (parite+1), r)
+	   else match r with
+	    |R _ -> a
+	    |L(,,_) -> L(c, l, aux x r (parite+1))
+	 else match l,r with
+	   |R ,R _|L(,_,_),L(,,_) ->
+	     if (leftorright()) = 0 then 
+	       L(c, aux x l (parite+1), r)
+	     else
+	       L(c, l, aux x r (parite+1))
+	   |R ,L(,_,_) -> L(c, aux x l (parite+1), r)
+	   |L(,,_),R _ -> L(c, l, aux x r (parite+1))
+      else
+	if (parite mod 2) <> 0 then	   
+	   if c.coord = x then a
+	   else if x<c.coord then match l with
+	    |R _ -> a
+	    |L(,,_) -> L(c, aux x l (parite+1), r)
+	   else match r with
+	    |R _ -> a
+	    |L(,,_) -> L(c, l, aux x r (parite+1)) 
+	else match l,r with
+	   |R ,R _|L(,_,_),L(,,_) ->
+	     if (leftorright()) = 0 then 
+	       L(c, aux x l (parite+1), r)
+	     else
+	       L(c, l, aux x r (parite+1))
+	   |R ,L(,_,_) -> L(c, aux x l (parite+1), r)
+	   |L(,,_),R _ -> L(c, l, aux x r (parite+1))	      				  
+  in aux x a 0
+;;
+
+(*fonction contruire un arbre bsp, prend un parametre int qui sera le nombre de fois qu'on repetera l'operation*)
+let cons_Bsp iter =
+  let rec aux a iter =
+    let noeud = random_noeud() in
+  match iter with
+  |(-1) -> a
+  |_ -> ajout_Bsp noeud (Random.int(1000)) (aux a (iter-1))
+  in aux (R None) iter 
 ;;
 
 
-let rec ajout e b = match b with
-	| R (a) -> L (e, R None, R None)
-	| L (l,_,_) when e=l ->b
-	| L (l,g,d) when l<e -> L (l, g, (ajout e d))
-	| L (l,g,d) -> L (l, (ajout e g), d);;
 
 
-let rec add e = function 
-	| R(a) -> L (e, R None, R None)
-	| L (l,g,d) -> if (rn e)<(rn l) then L (l, add e g, d)
-					else L (l, g, add e d)
-;;
-
-let rec ajoutS e b i p= match b with
-	| R(a) -> L (e, R None, R None)
-	| L (l,g,d) -> if (i mod 2) <> 0 then 
-		if (rn e)<(bspn b) then L (l, (ajoutS e g (hauteur g) b), d)
-					else L (l, g, (ajoutS e d (hauteur d) b))
-		else 
-			ajoutC e b i p
-and ajoutC e b i p = match b with
-	| R(a) -> L (e, R None, R None)
-	| L (l,g,d) -> if (i mod 2) = 0 then 
-		if (rn e)<(bspn b) then L (l, (ajoutC e g (hauteur g) b), d)
-					else L (l, g, (ajoutC e d (hauteur d) b))
-		else 
-			ajoutS e b i p 
-;;
-
-let ajout e bsp = 
-	let pater = ref R None and par = ref 0 in 
-		let aux add b c f = 
-			match c with
-				| R(a) -> L(c, R None, R None)
-				| L(l,g,d) -> if 
 
 
-let aj e b = ajoutS e b (hauteur b) (R None);;
-(ajout (n 500) (ajout (n 600) (ajout (n 750) (ajout (n 125) (ajout (n 200) (ajout (n 345) (ajout (n 760) (ajout (n 545) (R None) ))))))));;
-(add (n 500) (add (n 600) (add (n 750) (add (n 125) (add (n 200) (add (n 345) (add (n 760) (add (n 545) (R None) ))))))));;
-(aj (n 500) (aj (n 600) (aj (n 750) (aj (n 125) (aj (n 200) (aj (n 345) (aj (n 760) (aj (n 545) (R None) ))))))));;
-(addl (n 500) (addl (n 600) (addl (n 750) (addl (n 125) (addl (n 200) (addl (n 345) (addl (n 760) (addl (n 545) (R None) ))))))));;
+
+
+let rectangle x t w h l = 
+	let (a,b) = Graphics.currentt_point() and x1=x+w and y1 = y+h in
+		Graphics.moveto x y;
+		Grpahics.lineto x y1; Graphics.lineto x1 y1;
+		Graphics.lineto x1 y ; Graphics.lineto x y;
+		Graphics.moveto a b;
+		(a,b, x1, y1)::l;;
+	
+
+type rect =  int * int * int * int * bsp;;
+
+type ligne = int * int *int * int;;
+
+Graphics.lineto 100 100;;
+
+let getLine = 
+	let (a,b) = Graphics.current_point() in 
+		let aux = 
+			
+
+(* Dessiner un rectangle *)
+let rectangle1 x0 y0 w h l = 
+	let (a,b) = Graphics.current_point() and x1 = x0+w and y1 = y0+h in 
+		Graphics.moveto x0 y0; 
+		Graphics.lineto x0 y1; 
+		Graphics.lineto x1 y1; Graphics.lineto x1 y0; 
+		Graphics.lineto x0 y0; Graphics.moveto a b; 
+		(x0,y0, x1, y1, R None)::l;;
+
 
 (* Ajouter un label *)
 let lab b = match b with
     | R(a) -> { coord =0; colored= false; }
     | L(l,g,d) -> l;;
+
 
 
 let n a = {coord=a; colored=false};;
